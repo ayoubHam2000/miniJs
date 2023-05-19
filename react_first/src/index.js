099884cbf7dd948bf7facc24ab0a8d91ff183acc
aben-ham <ayoubbenhamou0731@gmail.com>
2023-05-19 14:59:56 +0100

    hhh

--- react_first/src/index.js
+++ react_first/src/index.js
@@ -1,14 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-function FirstComponenent() {
+function FirstComponenent(props) {
+  let listItems = ["dumpy", "pudgy", "tubby"];
+
+  const mapItems = listItems.map((item, index) => {
+    return <h1 key={index}>{item}</h1>;
+  });
+
   return (
-    <h2 class="asd" id="asd">
-      Hi!
-    </h2>
+    <React.Fragment>
+      <h1> {props.title} </h1>
+      <article>{mapItems}</article>
+    </React.Fragment>
   );
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<FirstComponenent />);
+root.render(<FirstComponenent title={"Hi"} />);
