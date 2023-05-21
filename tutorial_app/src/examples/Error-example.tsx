fa739f72b8c1994deefac265cd4e7a2c98d16c87
Your Name <you@example.com>
2023-05-21 11:59:12 +0100

    useReducer

--- tutorial_app/src/examples/Error-example.tsx
+++ tutorial_app/src/examples/Error-example.tsx
@@ -1,52 +1,33 @@
-import { useEffect, useState } from "react";
+import { useEffect, useReducer, useRef, useState } from "react";
 
-/*
-function Child(props: any) {
-  const { title } = props;
+function reducer(state: any, action: any) {
 
-  return (
-    <>
-    <div> {title} </div>
-    <button>  </button>
-    </>
-  )
-}*/
+}
+
+const data = {};
+
+const defaultState = {
+  data: data,
+  isLoading: false,
+};
 
 export function TestComponent() {
-  const arr = ["item1", "item2", "item3"];
+  const [state, dispatch] = useReducer(reducer, defaultState);
 
-  const [count, setCount] = useState(arr);
-  const [counter, setCounter] = useState(0);
 
-  const handleCountClick = (v: string) => {
-    count.splice(count.indexOf(v), 1);
-    console.log(v, count);
-    setCount([...count]);
-  };
-
-  const Child = (props: any) => {
-    const { title } = props;
-
-    return (
-      <>
-        <div> {title} </div>
-        <button onClick={() => handleCountClick(title)}> remove Item </button>
-      </>
-    );
-  };
-
-  useEffect(() => {
-    console.log("Hi!");
-  }, [counter]);
-
-  const childItems = count.map((value: string, index: number) => {
-    return <Child title={value} key={index} />;
-  });
+  const handleButton = () => {
+    dispatch({
+      type: "1",
+      payload: {
+        id: 1
+      }
+    })
+  }
 
   return (
-    <>
-      {childItems}
-      <button onClick={() => setCounter(counter + 1)}> Hi {counter} </button>
-    </>
-  );
+    <div>
+      Hi!
+    </div>
+    <button onClick={handleButton} ></button>
+  )
 }
